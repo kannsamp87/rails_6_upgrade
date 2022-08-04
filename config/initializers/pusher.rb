0392af59ada35e42bac3e291787666e1006a582f
kannsamp87 <kannan.sampath@freshworks.com>
2022-08-04 18:36:10 +0530

    first commit

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+require 'pusher'
+
+Pusher.app_id = '1456974'
+Pusher.key = '6310e121d37c5cc7395a'
+Pusher.secret = '63534ee23238e9d39352'
+Pusher.cluster = 'us3'
+Pusher.logger = Rails.logger
+Pusher.encrypted = true
