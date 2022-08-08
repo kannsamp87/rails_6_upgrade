a41bd96af9cd55ece8abdac3437e867782cc756a
kannsamp87 <kannan.sampath@freshworks.com>
2022-08-08 01:02:18 -0700

    Update pusher.rb

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,5 +1,7 @@
 require 'pusher'
 
+# Deleted this credentials to avoid the security leak, please replace with your app keys from dashboard.pusher.com
+
 Pusher.app_id = '1456974'
 Pusher.key = '6310e121d37c5cc7395a'
 Pusher.secret = '63534ee23238e9d39352'
